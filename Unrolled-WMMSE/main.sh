e588d35c094db2911356cce8a8d57c51d53451ae
RostyslavUA <olshevskyi966@gmail.com>
2024-12-09 20:18:54 +0000

    updated bash script

--- Unrolled-WMMSE/main.sh
+++ Unrolled-WMMSE/main.sh
@@ -5,8 +5,7 @@ thr=1
 for file in 'main.py' 'main_d.py' ; do
     for opt in 'adam' ; do
         for s in "set1_UMa_Optional_square_n25_lim500_thr${thr}_fading1" ; do
-#             for lr in 0.005 0.001 0.0005 ; do  # circle
-            for lr in 0.005 0.01 ; do  # square
+            for lr in 0.01 ; do  # square
                     lr_sci=$(awk "BEGIN {printf \"%.0e\",$lr}")
                     if [[ "${file}" == "main.py" ]] ; then
                         echo "Launch ${file} ${s} uwmmse train ${opt} lr ${lr_sci} ${reg_const}"
